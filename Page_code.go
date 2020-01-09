3c390ee9e9b0aa14bd6ea7e56c86cd9f1345e8d9
kvkram2405 <kvkram2405gmail.com>
2020-01-09 20:04:22 +0530

    Get username from a email address CODE is added

--- Page_code.go
+++ Page_code.go
@@ -3,10 +3,13 @@ package main
 import (
     "fmt"
     "net"
+	"strings"
 )
 
 // Split port and Host
 func main() {
-    host, port, err := net.SplitHostPort("127.0.0.1:5432")
-    fmt.Println(host, port, err)
-}
+   // host, port, err := net.SplitHostPort("127.0.0.1:5432")
+    //fmt.Println(host, port, err)
+	username := strings.Split("vkalipat@gmail.com", "@")
+ 	fmt.Printf("%q\n", username[0])
+ }
