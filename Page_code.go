ad6c64c2698231b853277e0740826b4e522dec77
kvkram2405 <kvkram2405gmail.com>
2020-01-09 20:07:58 +0530

    Updated old file

--- Page_code.go
+++ Page_code.go
@@ -6,10 +6,16 @@ import (
 	"strings"
 )
 
-// Split port and Host
+
 func main() {
-   // host, port, err := net.SplitHostPort("127.0.0.1:5432")
-    //fmt.Println(host, port, err)
+
+// Split port and Host
+
+   host, port, err := net.SplitHostPort("127.0.0.1:5432")
+   fmt.Println(host, port, err)
+
+// Split username and domain
+
 	username := strings.Split("vkalipat@gmail.com", "@")
  	fmt.Printf("%q\n", username[0])
- }
+ 	 }
