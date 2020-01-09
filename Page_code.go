0d24b08a65132c0123d5383befce0657f4f943f4
kvkram2405 <kvkram2405gmail.com>
2020-01-09 20:11:19 +0530

    Formated go fmt *.go

--- Page_code.go
+++ Page_code.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "net"
+	"fmt"
+	"net"
 	"strings"
 )
 
-
 func main() {
 
-// Split port and Host
+	// Split port and Host
 
-   host, port, err := net.SplitHostPort("127.0.0.1:5432")
-   fmt.Println(host, port, err)
+	host, port, err := net.SplitHostPort("127.0.0.1:5432")
+	fmt.Println(host, port, err)
 
-// Split username and domain
+	// Split username and domain
 
 	username := strings.Split("vkalipat@gmail.com", "@")
- 	fmt.Printf("%q\n", username[0])
- 	 }
+	fmt.Printf("%q\n", username[0])
+}
