e223dfa4ae9b032d308f55f1c6afa8c6ecfea8c2
kvkram2405 <kvkram2405gmail.com>
2020-01-16 01:33:39 +0530

    New co-ordinates Swap

--- Page_Distance_Between_Co-ordinates.go
+++ Page_Distance_Between_Co-ordinates.go
@@ -1,41 +1,41 @@
 package main
 
- import (
- 	"fmt"
- 	"math"
- )
+import (
+	"fmt"
+	"math"
+)
 
- type Coordinates struct {
- 	Latitude  float64
- 	Longitude float64
- }
+type Coordinates struct {
+	Latitude  float64
+	Longitude float64
+}
 
- const radius = 6371 // Earth's mean radius in kilometers
+const radius = 6371 // Earth's mean radius in kilometers
 
- func degrees2radians(degrees float64) float64 {
- 	return degrees * math.Pi / 180
- }
+func degrees2radians(degrees float64) float64 {
+	return degrees * math.Pi / 180
+}
 
- func (origin Coordinates) Distance(destination Coordinates) float64 {
- 	degreesLat := degrees2radians(destination.Latitude - origin.Latitude)
- 	degreesLong := degrees2radians(destination.Longitude - origin.Longitude)
- 	a := (math.Sin(degreesLat/2)*math.Sin(degreesLat/2) +
- 		math.Cos(degrees2radians(origin.Latitude))*
- 			math.Cos(degrees2radians(destination.Latitude))*math.Sin(degreesLong/2)*
- 			math.Sin(degreesLong/2))
- 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
- 	d := radius * c
+func (origin Coordinates) Distance(destination Coordinates) float64 {
+	degreesLat := degrees2radians(destination.Latitude - origin.Latitude)
+	degreesLong := degrees2radians(destination.Longitude - origin.Longitude)
+	a := (math.Sin(degreesLat/2)*math.Sin(degreesLat/2) +
+		math.Cos(degrees2radians(origin.Latitude))*
+			math.Cos(degrees2radians(destination.Latitude))*math.Sin(degreesLong/2)*
+			math.Sin(degreesLong/2))
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	d := radius * c
 
- 	return d
- }
+	return d
+}
 
- func main() {
- 	pointA := Coordinates{17.3850, 78.4867}
- 	pointB := Coordinates{12.9716, 77.5946}
- 	fmt.Println("Point A : ", pointA)
- 	fmt.Println("Point B : ", pointB)
+func main() {
+	pointA := Coordinates{78.4867, 17.3850}
+	pointB := Coordinates{12.9716, 77.5946}
+	fmt.Println("Point A : ", pointA)
+	fmt.Println("Point B : ", pointB)
 
- 	distance := pointA.Distance(pointB)
- 	fmt.Printf("The distance from point A to point B is %.2f kilometers.\n", distance)
+	distance := pointA.Distance(pointB)
+	fmt.Printf("The distance from point A to point B is %.2f kilometers.\n", distance)
 
- }
+}
---
+++ Page_Mysql_extractdata.go
@@ -0,0 +1,43 @@
+package main
+ 
+import (
+    "fmt"
+    "log"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+)
+ 
+func main() {
+    db, err := sql.Open("mysql", "root:root@123@/gosql")
+    defer db.Close()
+ 
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+ 
+    err = db.Ping()
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+ 
+ 
+    rows, err := db.Query("SELECT COUNT(*) AS total FROM employee")
+    defer rows.Close()
+ 
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+ 
+    for rows.Next() {
+        var total int
+        if err := rows.Scan(&total); err != nil {
+                log.Fatal(err.Error())
+        }
+        fmt.Printf("Total rows found = %d\n", total)
+    }
+    if err := rows.Err(); err != nil {
+        log.Fatal(err.Error())
+    }
+ 
+ 
+}
